a39e21049118aa056c501d5233c276591193d846
FIC <fic@info.local>
2025-08-22 21:57:19 -0300

    continuando

---
+++ javascripit/condicionais.js
@@ -0,0 +1,10 @@
+//ex 1 //
+
+let idade = prompt("qual a idade?");
+if (idade>=18){
+    alert("parabèns! você e maior de idade.") 
+    
+} else {
+    alert("vai pra Super Nani!");
+
+}
---
+++ javascripit/script.js
@@ -0,0 +1,16 @@
+// console.log ("hello word!")
+
+// let nome = prompt ("qual seu nome?")
+
+// let idade = prompt ("qual a sua idade ?" )
+
+// alert("meu nome e " + nome + " e minha idade e " + idade )
+
+let idade = prompt("qual a idade?");
+if (idade>=18){
+    alert("parabèns! você e maior de idade.") 
+    
+} else {
+    alert("vai pra Super Nani!");
+
+}
