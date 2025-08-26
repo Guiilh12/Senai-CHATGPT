ff75c1cbc19854f2ac0dcdb3f77dbf371c96ec72
FIC <fic@info.local>
2025-08-25 21:27:43 -0300

    concluido

--- javascripit/condicionais.js
+++ javascripit/condicionais.js
@@ -1,10 +1,102 @@
-//ex 1 //
+//exercicio 1 //
 
-let idade = prompt("qual a idade?");
-if (idade>=18){
-    alert("parabèns! você e maior de idade.") 
-    
-} else {
-    alert("vai pra Super Nani!");
+//let idade = prompt("qual a idade?");
+//if (idade>=18){
+// alert("parabèns! você e maior de idade.") 
 
-}
+//} else {
+// alert("vai pra Super Nani!");
+
+//exercicio 2
+
+//  let numero = prompt("digite um número.");
+//  if ( numero >= 0) {
+//     alert ("positivo.");
+//  }else {
+//     alert ("Negativo.");
+//  }
+
+
+//exercicio 3
+// let nummero = prompt(" insere a nota de 0 a 100");
+
+// if ( nummero >= 60 ) {
+
+// alert( "aprovado." );
+
+// }else{
+
+//     alert( "Reprovado. ");
+// }
+
+
+//exercicio 4
+
+// let numero = prompt("digie um número");
+// if (numero > 0) {
+//     alert("Positivo.");
+// } else {
+
+//     if (numero < 0) {
+//         alert("Negativo.");
+//     } else {
+//         alert("Zero.");
+//     }
+// }
+
+// exercicio 5
+
+// let idade = prompt("insira a sua idade");
+// if ( idade > 0 && idade <= 12 ){
+//     alert("Criança.");
+// } else if (idade >= 13 && idade <= 17){
+//     alert("Adolecente.");
+// }else{
+//     alert("Adulto.")
+// }
+
+// exercicio 6 
+// let numero = prompt(" inserir um número");
+
+// if ( numero % 2== 0){
+//     alert("Par.")
+// }else{
+//     alert("Impar.")
+// }
+
+
+
+// exercicio 7
+
+
+// let numero1 = Number (prompt ("insira um número"));
+
+// let operacao = prompt("operação(+, -, *, /).");
+
+// let numero2 = Number  (prompt ("insira um número"));
+
+// if (operacao == "+"){
+//     alert( numero1 + numero2 );
+// }
+
+// else if (operacao == "-"){
+//     alert( numero1 - numero2  );
+// }
+
+
+// else if (operacao == "*"){
+//     alert( numero1 * numero2  );
+
+// }else if (operacao == "/"){
+//     alert( numero1 / numero2  );
+// }
+
+// exercicio 8
+
+// let usuario = prompt("usuario");
+// let senha = prompt("senha");
+// if(usuario == "admin" & senha == "1234"){
+//     alert("Login bem-sucedido")
+// } else{
+//     alert("Acesso negado")
+// }
