068c86d3725ce305f72faf29f0ec2def54d08a84
FIC <fic@info.local>
2025-08-27 21:55:51 -0300

    repeticoes

---
+++ javascripit/repeticoes.js
@@ -0,0 +1,36 @@
+// console.log ("FOr")
+// for (let contadora = 1; contadora <= 5; contadora++){
+//     console.log(contadora)
+// }
+
+// console.log("while");
+// let contadorawhile=1;
+// while(contadorawhile <=5){
+//     console.log(contadorawhile);
+//     contadorawhile++
+// }
+
+// console.log("do while simples ")
+//  do{
+//     console.log("mostrar o valor  na  tela, ainda que a condiçao seja falsa. ")
+//  }while( 1 == 2 );
+
+// Contagem de 1 a 10
+
+// for (i = 1; i <= 10; i++)
+//     console.log(i)
+
+// Tabuada de um número
+// let numero = ( prompt("número"))
+// for (i = 1; i <= 10; i++){
+// console.log( numero + "x" + i + "=" + (numero *i));
+// }
+
+// Soma dos primeiros N números naturais
+
+// let numero = ( prompt("número"))
+// let soma = 0
+
+// for ( let i=1; i<= numero;i++ ){
+//     soma = soma + i;
+// } console.log(soma)
